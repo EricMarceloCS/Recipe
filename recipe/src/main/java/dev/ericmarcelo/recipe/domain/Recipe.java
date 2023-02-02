4071e2849ca539314619eaae8251ed32910b3d7a
eric <ericmarcelo455@gmail.com>
2023-02-02 13:15:09 -0800

    JPA
    * Using jakarta.persistence
    * Set up JPA relationships between entities
    * Created Join Table
    * Created H2 database

--- recipe/src/main/java/dev/ericmarcelo/recipe/domain/Recipe.java
+++ recipe/src/main/java/dev/ericmarcelo/recipe/domain/Recipe.java
@@ -1,4 +1,6 @@
 package dev.ericmarcelo.recipe.domain;
+import java.util.Set;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -15,10 +17,25 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	//todo
-	//private Difficulty difficulty;
+	
+	@Enumerated(value =EnumType.STRING)
+	private Difficulty difficulty;
+	
 	@Lob
 	private Byte[] image;
+	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+	
+	@ManyToMany
+	@JoinTable(name="recipe_category", 
+		joinColumns = @JoinColumn(name = "recipe_id"),
+		inverseJoinColumns=@JoinColumn(name ="category_id"))
+	private Set<Category> categories;
 	
 	public Long getId() {
 		return id;
@@ -26,8 +43,6 @@ public class Recipe {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	@OneToOne(cascade = CascadeType.ALL)
-	private Notes notes;
 	
 	public String getDescription() {
 		return description;
@@ -82,6 +97,24 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public Set<Category> getCatogories() {
+		return categories;
+	}
+	public void setCatogories(Set<Category> catogories) {
+		this.categories = catogories;
 	}
 
 	
